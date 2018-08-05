0ea214058a98460cfa9be25ad30359eee6d408d7
Steve Peak <steve@stevepeak.net>
2018-08-05 11:40:06 +0200

    update brewery to use direct tar.gz

--- scripts/brewery.py
+++ scripts/brewery.py
@@ -10,18 +10,26 @@ import re
 import sys
 import requests
 
-resource = '''
-resource "{name}" do
-    url "https://pypi.python.org/packages/source/{name[0]}/{name}/{name}-{version}.tar.gz"
-    sha256 "{sha}"
+
+resource = '''resource "{name}" do
+  url "https://files.pythonhosted.org/{url}.tar.gz"
+  sha256 "{sha}"
 end
-'''.strip()
+'''
 
-pattern = re.compile(r'data-clipboard-text="(\w{50,})"')
+sha_pattern = re.compile(
+    r'data-clipboard-text="(\w{50,})"'
+)
 
-for dep in sys.stdin.read().split():
-    assert '==' in dep, 'Must specify exact version'
-    name, version = tuple(dep.split('=='))
-    res = requests.get(f'https://pypi.org/project/{name}/{version}')
-    sha = pattern.findall(res.text)[-1]
-    print(resource.format(name=name, version=version, sha=sha))
+pkg_pattern = re.compile(
+    r'<a href="https://files.pythonhosted.org/(.*).tar.gz">'
+)
+
+if __name__ == '__main__':
+    for dep in sys.stdin.read().split():
+        assert '==' in dep, 'Must specify exact version'
+        name, version = tuple(dep.split('=='))
+        res = requests.get(f'https://pypi.org/project/{name}/{version}')
+        url = pkg_pattern.findall(res.text)[-1]
+        sha = sha_pattern.findall(res.text)[-1]
+        sys.stdout.write(resource.format(name=name, url=url, sha=sha))
